7b0617dd936ffadf4203aad91e1215b0445facf4
Caitlin Hines and Nicole Sanders <caitlin@>
2017-01-17 14:48:56 -0800

    add factorial function

--- js/scripts.js
+++ js/scripts.js
@@ -4,14 +4,17 @@ $("document").ready(function(){
 
     var endValue = parseInt($("#end").val());
     var multipleValue = parseInt($("#increment").val());
+    var factValue = parseInt($("#factorial").val());
 
     $("#result").append(countBy(endValue, multipleValue))
+
+    $("#factResult").append(factorialOutput(factValue))
   })
 })
 
 var countBy = function(endValue, multipleValue){
   if (endValue && multipleValue){
-    var output = ""
+    var output = "";
     for (var number = 0; number <= endValue; number += multipleValue){
       output += number + ", ";
     }
@@ -20,3 +23,12 @@ var countBy = function(endValue, multipleValue){
     alert("You need to put a value in all boxes");
   }
 };
+
+var factorialOutput = function(factorial) {
+  if (factorial) {
+    var answer = 1;
+    for (var number = 1; number <= factorial; number += 1) {
+      answer *= number;
+    }
+  }return answer;
+}
